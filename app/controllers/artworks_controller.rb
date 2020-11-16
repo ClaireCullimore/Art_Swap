5bf9516c41a93efd3125977a9a8839a0e23fa8d7
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-16 17:51:49 +0000

    finished controller and basic pages

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,30 @@
+class ArtworksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  def index
+    @artworks = Artwork.all
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
+    if @artwork.save!
+      redirect_to artwork_path(@artwork)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :artist, :category, :description)
+  end
+end
