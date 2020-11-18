b0c1c60be20fb19c67fe6362cf808c709390c6fc
CLaire Cullimore <cascullimore@gmail.com>
2020-11-18 10:48:07 +0000

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :artworks, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :artworks do
+    resources :bookings, only: [:create]
+  end
 end
